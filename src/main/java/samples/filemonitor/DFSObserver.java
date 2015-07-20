557d83de7c49add114e9bc5eb8f41ca971873cb6
Jaime Spicciati <jaimespicciati@Jaimes-MBP.home>
2015-07-19 22:45:51 -0400

    wip of dfs implementation

---
+++ src/main/java/samples/filemonitor/DFSObserver.java
@@ -0,0 +1,5 @@
+package samples.filemonitor;
+
+public class DFSObserver {
+
+}
---
+++ src/test/java/samples/filemonitor/DFSMonitorTest.java
@@ -0,0 +1,14 @@
+package samples.filemonitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DFSMonitorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
